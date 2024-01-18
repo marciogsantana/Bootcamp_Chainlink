27f78b1c5d8536d7e055b90fd3198aacdfdfb47e
Márcio Gomes de Santana <68255679+marciogsantana@users.noreply.github.com>
2024-01-17 22:14:38 -0300

    Update CrossDestinationMinter.sol

--- CrossDestinationMinter.sol
+++ CrossDestinationMinter.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.19;
 
 // Deploy this contract on Sepolia
+// use address NFT for deploy CrossChain`PriceNFT
 
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
